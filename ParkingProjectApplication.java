7381d105b103b43a09aa876aa214a0346b75ae90
abhijitWa <140107345+abhijitWa@users.noreply.github.com>
2023-08-06 14:38:58 +0530

    Create ParkingProjectApplication.java

---
+++ ParkingProjectApplication.java
@@ -0,0 +1,14 @@
+package com.parkyee_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingProjectApplication.class, args);
+		System.err.println("parkyee");
+	}
+
+}
